0355d6a6232e9efc4cf37fbf98a6894e72d7f40d
Tindtily <283893159@qq.com>
2018-07-08 16:41:04 +0800

    change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
-gulp.task('default', ['praise'], () =>
-    gulp.watch(['src/**/*.es6', '!src/public/*/*.es6'],['praise'])
-);
+// gulp.task('default', ['praise'], () =>
+//     gulp.watch(['src/**/*.es6', '!src/public/*/*.es6'],['praise'])
+// );
 
-gulp.task('praise', () => {
+gulp.task('default', () => {
     return gulp.src(['src/**/*.es6', '!src/public/*/*.es6'])
         .pipe(babel({
             presets: ['env']
